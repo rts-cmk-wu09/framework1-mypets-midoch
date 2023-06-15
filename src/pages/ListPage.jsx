afba676408884e1ce8726f5457f88c84478ac8aa
mido <101448145+midoch@users.noreply.github.com>
2023-06-15 11:24:38 +0200

    Update ListPage.jsx

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,39 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useAxios from "../useAxios";
+import Header from "../components/HomePageHeader";
+import HeroSection from "../components/HomePageHeroSection";
+import Footer from "../components/HomePageFooter";
 import "../styles/ListPage.css";
-import fetchPets from "../API";
 
-const ListPage = () => {
-  const [pets, setPets] = useState([]);
-
-  useEffect(() => {
-    const fetchPetsData = async () => {
-      try {
-        const data = await fetchPets();
-        setPets(data);
-      } catch (error) {
-        console.log("Error fetching pets:", error);
-      }
-    };
-
-    fetchPetsData();
-  }, []);
+const ListPage = (props) => {
+  const [data, error, loading] = useAxios();
 
   return (
-    <div className="list-page">
-      <h1 className="list-page-title">Available Pets</h1>
-      <ul className="pets-list">
-        {pets.map((pet) => (
-          <li key={pet.id} className="pet-item">
-            <img src={pet.image} alt={pet.name} className="pet-image" />
-            <div className="pet-details">
-              <h2 className="pet-name">{pet.name}</h2>
-              <p className="pet-location">{pet.location}</p>
-              <p className="pet-description">{pet.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <HeroSection />
+      <Footer />
+    </>
   );
 };
 
