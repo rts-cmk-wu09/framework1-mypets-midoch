b701cf33746dab9399d378ca75707b4fa27a6311
mido <101448145+midoch@users.noreply.github.com>
2023-06-15 11:24:20 +0200

    Create HomePageFooter.jsx

---
+++ src/components/HomePageFooter.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="homepage-footer">
+      <p>© 2023 Pet Finder. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
