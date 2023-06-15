e0a6ba28b45ad84b340318b817a1b314f036bfbe
mido <101448145+midoch@users.noreply.github.com>
2023-06-15 11:24:23 +0200

    Create HomePageHeader.jsx

---
+++ src/components/HomePageHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="homepage-header">
+      <h1 className="homepage-title">Welcome to Pet Finder</h1>
+      <p>Find your perfect pet companion!</p>
+    </header>
+  );
+};
+
+export default Header;
